99452bc80f8354b75d9ae8d86de1a5590c572cf0
Azmarie <ywa194@sfu.ca>
2018-03-28 10:49:54 -0700

    change aboutus div

--- react-fullstack/src/routes/home/Home.js
+++ react-fullstack/src/routes/home/Home.js
@@ -28,6 +28,7 @@ function Home({ news }) {
       desc: 'foobar desc text',
       contentSnippet: '<div>foobar</div>'
     }
+
   ]);
 
   // <span
@@ -62,7 +63,9 @@ function Home({ news }) {
         </div>
         <div className={s.aboutus}>
           <h2 className={s.title}>About Us</h2>
-            <p>We are a team from CMPT 470, SFU. This website is to collect news, artiles from the Internet for educational purpose. We do concern about the copyright for every artiles we displayed. If you find there is any material which infringes your copyright, please let us know by email and provide the details (such as URL, your origin work, etc.). We will delete every infringements as soon as possible.</p>
+            <p className={s.info}>We are a team from CMPT 470, SFU. This website is to collect news, artiles from the Internet for educational purpose. We do concern about the copyright for every artiles we displayed. If you find there is any material which infringes your copyright, please let us know by email and provide the details (such as URL, your origin work, etc.). We will delete every infringements as soon as possible.</p>
+            <p className={s.email}><svg width="16" height="16" xmlns="http://www.w3.org/2000/svg"><path d="M2.167 2h11.666C14.478 2 15 2.576 15 3.286v9.428c0 .71-.522 1.286-1.167 1.286H2.167C1.522 14 1 13.424 1 12.714V3.286C1 2.576 1.522 2 2.167 2zm-.164 3v1L8 10l6-4V5L8 9 2.003 5z" fill="#6a8" fill-rule="evenodd"></path></svg><a href="mailto:cmpt470-ai@sfu.ca" target="_blank">CMPT470-AI@sfu.ca</a></p>
+
         </div>
       </div>
     </div>
