b76e05407944dba1b4701136ee3d4a7a0dcd17d4
Azmarie <ywa194@sfu.ca>
2018-03-28 21:59:21 -0700

    [server] Integrated server code into ui code

---
+++ react-fullstack/src/server/new.js
@@ -0,0 +1,34 @@
+var fs = require('fs');
+const NewsAPI = require('newsapi');
+const newsapi = new NewsAPI('73f4cef40a3c444c9040fa8a54189546');
+
+exports.getNews = function() {
+// To query /v2/top-headlines
+// All options passed to topHeadlines are optional, but you need to include at least one of them
+newsapi.v2.topHeadlines({
+  //sources: 'bbc-news,the-verge',
+  //q: 'bitcoin',
+  category: 'technology',
+  language: 'en',
+  country: 'us'
+}).then(response => {
+  //console.log(response);
+  
+  //var newRes = response.articles;
+  //console.log(newRes);
+
+  fs.writeFile('news.json', JSON.stringify(response, null,4), function(err){
+        console.log('write file success!')
+      });
+
+  //console.log(response.articles[0].url);
+  
+  /*
+    {
+      status: "ok",
+      articles: [...]
+    }
+*/  
+});
+
+};
