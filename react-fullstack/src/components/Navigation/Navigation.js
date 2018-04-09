3ff0dd393f2771dcf02417885122f6c99481cca4
Azmarie <ywa194@sfu.ca>
2018-04-08 21:00:49 -0700

    [UI] Removed login links in the navigation header

--- react-fullstack/src/components/Navigation/Navigation.js
+++ react-fullstack/src/components/Navigation/Navigation.js
@@ -7,9 +7,6 @@ import Link from '../Link';
 function Navigation({ className }) {
   return (
     <div className={cx(s.root, className)} role="navigation">
-      <Link className={s.link + ' ' + s.font} to="/login">Log in</Link>
-      <span className={s.spacer}> | </span>
-      <Link className={cx(s.link, s.highlight)} to="/register">Sign up</Link>
     </div>
   );
 }
